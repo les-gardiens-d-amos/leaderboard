5c5d570b80ebd3d0d7a0aae284ac1553fc9be299
kirua <yo44prg@icloud.com>
2021-11-24 21:47:21 +0100

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <!-- <img alt="Amos Guardian" src="./assets/guardian-amos-logo.png"> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
+:root {
+  --white: #ffffff;
+  --primary_c: #495E78;
+  --secondary_c: #67697C;
+  --tertiary_c: #969696;
+  --quaternary_c: #B79D94;
+  --warning_c: #DDB771;
+  --error_c: #A30B37;
+  --success_c: #5cd65c;
+  --mammal_c: #F887B0;
+  --bird_c: #7EDCE6;
+  --fish_c: #3289F6;
+  --amphibian_c: #63BC55;
+  --reptile_c: #2D8159;
+  --invertebrate_c: #783BB6;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  min-height: 300vh;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,97 @@
+<template>
+  <header class="header">
+      <div class="logo-container">
+        <div class="image-logo-container">
+            <img alt="Amos Guardian" src="../assets/guardian-amos-logo.png" class="guardian-logo" />
+        </div>
+        <h1 class="title">Les Gardiens d'Amos</h1>
+      </div>
+      <nav class="nav-container">
+          <ul>
+              <li>
+                  <p class="nav-link">leaderboard</p>
+              </li>
+              <li>
+                  <p class="nav-link">statistiques</p>
+              </li>
+          </ul>
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.header {
+    width: 100%;
+    height: 60px;
+    background-color: var(--primary_c);
+    color: var(--white);
+    display: flex;
+    justify-content: space-between;
+    position: fixed;
+}
+
+.logo-container {
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.image-logo-container {
+    width: 14%;
+    margin-top: 45px;
+    background-color: var(--white);
+    border: 4px solid var(--primary_c);
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 54px;
+}
+
+.guardian-logo {
+    height: 100px;
+    width: 100px;
+}
+
+.title {
+    width: 70%;
+    padding-left: 20px;
+}
+
+.nav-container {
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.nav-container ul {
+    width: 50%;
+    list-style-type: none;
+    display: flex;
+    flex-direction: row;
+}
+
+.nav-container ul > li {
+    margin-left: 25px;
+}
+
+.nav-link {
+    padding: 10px;
+    border-radius: 10px;
+    transition: 0.5s;
+}
+
+.nav-link:hover {
+    cursor: pointer;
+    background-color: var(--white);
+    color: var(--primary_c);
+}
+</style>
