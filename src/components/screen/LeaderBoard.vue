74fe2cae260fee6914909248fb7ae2fb02105184
kirua <yo44prg@icloud.com>
2021-11-25 11:55:55 +0100

    feat: add real data in leaderboard

--- src/components/screen/LeaderBoard.vue
+++ src/components/screen/LeaderBoard.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="leaderboard">
       <h1 class="leaderboard-title">LeaderBoard</h1>
-      <v-data-table
+      <v-data-table v-if="leaders != null"
         :headers="headers"
-        :items="desserts"
-        :items-per-page="5"
+        :items="leaders"
+        :items-per-page="10"
         class="elevation-1"
       ></v-data-table>
   </div>
@@ -16,101 +16,31 @@
       return {
         headers: [
           {
-            text: 'Dessert (100g serving)',
+            text: 'LeaderBoard',
             align: 'start',
             sortable: false,
             value: 'name',
           },
-          { text: 'Calories', value: 'calories' },
-          { text: 'Fat (g)', value: 'fat' },
-          { text: 'Carbs (g)', value: 'carbs' },
-          { text: 'Protein (g)', value: 'protein' },
-          { text: 'Iron (%)', value: 'iron' },
+          { text: "Score", value: "score" },
+          { text: "Nombre d'amos", value: "amos_count" },
+          { text: "Nombre de type d'amos découvert", value: "amos_type_count" },
+          { text: "Niveau total des amos", value: "total_amos_level" },
+          { text: "Type d'amos préferé", value: "amos_type_prefer" },
+          { text: "Date de la dernière capture", value: "last_catch" }
         ],
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%',
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1%',
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%',
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%',
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%',
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%',
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%',
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45%',
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22%',
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6%',
-          },
-        ],
+        leaders: null
       }
     },
+    beforeMount() {
+      this.$axios.get("https://happy-amos.herokuapp.com/leaderboard").then(response => {
+        this.leaders = response.data.leaderboard;
+        for (let i = 0; i < this.leaders.length; i++) {
+          this.leaders[i].last_catch = this.$moment(this.leaders[i].last_catch).fromNow();
+        }
+      }).catch(error => {
+        console.log(error);
+      });
+    }
   }
 </script>
 
@@ -126,5 +56,6 @@
 
 .elevation-1 {
   width: 100%;
+  margin-top: 20px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import VueRouter from 'vue-router'
 
 import routes from './router.js';
 
+import axios from "axios";
+import moment from "moment";
+
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
 Vue.use(VueRouter);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.prototype.$axios = axios;
+Vue.prototype.$moment = moment;
 
 const router = new VueRouter({ routes })
 
