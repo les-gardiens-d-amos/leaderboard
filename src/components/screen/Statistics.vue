cc702b1f13247f74cc25eebb5290aac232d8874d
Kirua <yo44prg@icloud.com>
2021-11-29 14:36:40 +0100

    feat: externalize component map

--- src/components/screen/Statistics.vue
+++ src/components/screen/Statistics.vue
@@ -1,45 +1,32 @@
 <template>
-  <v-card class="map-container">
-    <l-map class="map" :zoom="zoom" :center="center">
-      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-      <l-marker :lat-lng="markerLatLng"></l-marker>
-    </l-map>
-  </v-card>
+  <div class="stats-container">
+    <h1 class="stats-title">Statistiques</h1>
+    <Map/>
+  </div>
 </template>
 
 <script>
-import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
+import Map from "./statitsics/Map.vue";
 
 export default {
   components: {
-    LMap,
-    LTileLayer,
-    LMarker
-  },
-  data () {
-    return {
-      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      attribution:'&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      zoom: 15,
-      center: [51.505, -0.159],
-      markerLatLng: [51.504, -0.159]
-    };
+    Map
   }
 }
 </script>
 
-<style scoped>
-.map-container {
-  width: 75%;
+<style>
+.stats-container {
+  width: 100%;
   display: flex;
   justify-content: space-around;
   flex-wrap: wrap;
 }
 
-.map {
+.stats-title {
   width: 100%;
-  height: 450px;
-  overflow: hidden;
-  float: right;
+  text-align: center;
+  color: var(--secondary_c);
+  margin-bottom: 15px;
 }
 </style>
---
+++ src/components/screen/statitsics/Map.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-card class="map-container">
+    <l-map class="map" :zoom="zoom" :center="center">
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <l-marker :lat-lng="markerLatLng"></l-marker>
+    </l-map>
+  </v-card>
+</template>
+
+<script>
+import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
+  },
+  data () {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:'&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 15,
+      center: [51.505, -0.159],
+      markerLatLng: [51.504, -0.159]
+    };
+  }
+}
+</script>
+
+<style scoped>
+.map-container {
+  width: 75%;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+
+.map {
+  width: 100%;
+  height: 450px;
+  overflow: hidden;
+  float: right;
+}
+</style>
